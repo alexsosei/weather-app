c37691f37b1558283d5385573f3fb45d194a67b9
[alexander osei] <[valid.alexsosei@icloud.com]>
2023-02-07 10:18:18 +0000

    Current time and date

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,2 +1,7 @@
 var key = '246deb70d3ed5b4b5fb97fc32ae651df';
 var city = "London"
+
+//Pin Time and location 
+
+var date = moment().format('ddd, MMMM Do YYYY');
+var dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
